7a084bca785b92df9b968ad5089140fac62df78b
Bagher Hussaini <62519081+bagherhussaini@users.noreply.github.com>
2021-10-15 22:50:39 +0330

    Update exercise-8-lcm.py

--- assignment-3/exercise-8-lcm.py
+++ assignment-3/exercise-8-lcm.py
@@ -2,8 +2,7 @@ num1, num2 = int(input('Enter 1st Number: ')), int(input('Enter 2nd Number: '))
 lcm = 0
 while num1 == 0 or num2 == 0:
     print('Don\'t Enter Zero :|')
-    num1 = int(input('Enter 1st Number: '))
-    num2 = int(input('Enter 2nd Number: '))
+    num1, num2 = int(input('Enter 1st Number: ')), int(input('Enter 2nd Number: '))
 greater = max(abs(num1), abs(num2))
 for i in range(greater, abs(num1) * abs(num2) + 1):
     if (i % num1 == 0) and (i % num2 == 0):
