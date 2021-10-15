b3f541dff5895443c9da79d27d050dfac8b57d97
Bagher Hussaini <62519081+bagherhussaini@users.noreply.github.com>
2021-10-15 22:44:14 +0330

    Update exercise-6-check-ascending-array.py

--- assignment-3/exercise-6-check-ascending-array.py
+++ assignment-3/exercise-6-check-ascending-array.py
@@ -1,8 +1,7 @@
 numbers_list = []
 list_length = int(input('Enter the Length of Array(List): '))
 while list_length <= 0:
-    list_length = int(
-        input('Don\'t Enter Negative or Zero :|\nEnter the Length of Array(List): '))
+    list_length = int(input('Don\'t Enter Negative or Zero :|\nEnter the Length of Array(List): '))
 for i in range(list_length):
     print('Enter Element[', i+1, ']: ', end='')
     numbers_list.append(int(input()))
