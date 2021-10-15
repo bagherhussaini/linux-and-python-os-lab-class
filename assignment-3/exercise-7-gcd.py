a75f88b15be18cf50f0f083eb1c356631970e777
Bagher Hussaini <62519081+bagherhussaini@users.noreply.github.com>
2021-10-15 22:49:59 +0330

    Update exercise-7-gcd.py

--- assignment-3/exercise-7-gcd.py
+++ assignment-3/exercise-7-gcd.py
@@ -2,8 +2,7 @@ num1, num2 = int(input('Enter 1st Number: ')), int(input('Enter 2nd Number: '))
 gcd = 0
 while num1 == 0 or num2 == 0:
     print('Don\'t Enter Zero :|')
-    num1 = int(input('Enter 1st Number: '))
-    num2 = int(input('Enter 2nd Number: '))
+    num1, num2 = int(input('Enter 1st Number: ')), int(input('Enter 2nd Number: '))
 smaller = min(abs(num1), abs(num2))
 for i in range(1, smaller + 1):
     if (num1 % i == 0) and (num2 % i == 0):
