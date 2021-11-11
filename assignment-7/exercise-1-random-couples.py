9fba77078c3fc70148e8ea96d47f9a1c13263d57
Bagher Hussaini <62519081+bagherhussaini@users.noreply.github.com>
2021-11-12 01:12:24 +0330

    Update exercise-1-random-couples.py

--- assignment-7/exercise-1-random-couples.py
+++ assignment-7/exercise-1-random-couples.py
@@ -3,17 +3,13 @@ boys = ['ali', 'reza', 'yasin', 'benyamin', 'mehrdad', 'sajjad', 'aidin', 'shahi
 girls = ['sara', 'zari', 'neda', 'homa', 'eli', 'goli', 'mary', 'mina']
 random_boys = []
 random_girls = []
-for i in range(len(boys)):
-    while len(random_boys) != (len(boys)):
-        r1 = randint(0, len(boys) - 1)
-        if boys[r1] not in random_boys:
-            random_boys.append(boys[r1])
-
-for i in range(len(girls)):
-    while len(random_girls) != len(girls):
-        r2 = randint(0, len(girls) - 1)
-        if girls[r2] not in random_girls:
-            random_girls.append(girls[r2])
-
+while len(random_boys) != (len(boys)):
+    r1 = randint(0, len(boys) - 1)
+    if boys[r1] not in random_boys:
+        random_boys.append(boys[r1])
+while len(random_girls) != len(girls):
+    r2 = randint(0, len(girls) - 1)
+    if girls[r2] not in random_girls:
+        random_girls.append(girls[r2])
 for i in range(len(boys)):
     print(random_boys[i], ' ❤️', random_girls[i])
