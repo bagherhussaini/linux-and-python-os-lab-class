d592797db8f7e0c0c21591a1060573ca827b1896
Bagher Hussaini <62519081+bagherhussaini@users.noreply.github.com>
2021-11-20 14:14:02 +0330

    little modified

--- assignment-7/exercise-1-random-couples.py
+++ assignment-7/exercise-1-random-couples.py
@@ -1,15 +1,13 @@
 from random import randint
 boys = ['ali', 'reza', 'yasin', 'benyamin', 'mehrdad', 'sajjad', 'aidin', 'shahin']
 girls = ['sara', 'zari', 'neda', 'homa', 'eli', 'goli', 'mary', 'mina']
-random_boys = []
-random_girls = []
-while len(random_boys) != (len(boys)):
-    r1 = randint(0, len(boys) - 1)
-    if boys[r1] not in random_boys:
-        random_boys.append(boys[r1])
-while len(random_girls) != len(girls):
-    r2 = randint(0, len(girls) - 1)
-    if girls[r2] not in random_girls:
-        random_girls.append(girls[r2])
-for i in range(len(boys)):
-    print(random_boys[i], ' ❤️', random_girls[i])
+size, random_boys, random_girls = len(boys), [], []
+while len(random_boys) != size or len(random_girls) != size:
+    random_boy = randint(0, size - 1)
+    if boys[random_boy] not in random_boys:
+        random_boys.append(boys[random_boy])
+    random_girl = randint(0, size - 1)
+    if girls[random_girl] not in random_girls:
+        random_girls.append(girls[random_girl])
+for i in range(size):
+    print('(', random_boys[i], ',',  random_girls[i], ')', end='')
